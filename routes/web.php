b6341cb180cc34c8adba024c441248d6fb80fcd0
SyazaitulMNR <syazaitulmnr@gmail.com>
2021-11-08 15:15:58 +0800

    081121_2route

--- routes/web.php
+++ routes/web.php
@@ -20,23 +20,18 @@ use App\Http\Controllers\CarController;
 
 // Route::resource('cars', CarController::class);
 Route::get('/',[ CarController::class, 'index'])->name('index');
-Route::get('cars/search',[ CarController::class, 'indexSearch' ])->name('FindMeNow/search');
+Route::get('/search',[ CarController::class, 'indexSearch' ])->name('FindMeNow/search');
 // Route::get('cars/carlist', 'CarController@allList');
-Route::get('cars/carlist',[ CarController::class, 'allList' ])->name('FindMeNow/all');
+Route::get('/carlist',[ CarController::class, 'allList' ])->name('FindMeNow/all');
 
 //for create
-Route::get('cars/create',[ CarController::class, 'create' ])->name('FindMeNow/create');
-Route::post('cars/store',[ CarController::class, 'store' ])->name('FindMeNow/store');
+Route::get('/create',[ CarController::class, 'create' ])->name('FindMeNow/create');
+Route::post('/store',[ CarController::class, 'store' ])->name('FindMeNow/store');
 
 //for edit details
-Route::get('cars/edit/{noplate}',[ CarController::class, 'edit' ])->name('FindMeNow/edit');
-Route::get('cars/update/{noplate}',[ CarController::class, 'update' ])->name('FindMeNow/update');
+// Route::get('/edit/{noplate}',[ CarController::class, 'edit' ])->name('FindMeNow/edit');
+// Route::get('/update/{noplate}',[ CarController::class, 'update' ])->name('FindMeNow/update');
 //for view details only
-Route::get('cars/show/{noplate}',[ CarController::class, 'show' ])->name('FindMeNow/show');
+Route::get('/show/{noplate}',[ CarController::class, 'show' ])->name('FindMeNow/show');
 //search NO PLATE function
-Route::get('cars/result/', [CarController::class, 'search' ])->name('FindMeNow/result');
-
-// Route::get('/CariKereta//{id}',[ CarController::class, 'edit'])->name('CariKerete/edit');
-// Route::post('CariKereta/create', 'CarController@store');
-
-// Route::get('cars/carlist', 'CarController@test');
+Route::get('/result/', [CarController::class, 'search' ])->name('FindMeNow/result');
